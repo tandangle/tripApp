f274c5659c8dc0fe28a9b1d17cafd750158ec019
Aaron Hegde <hegde.aaron@gmail.com>
2020-05-18 17:13:48 -0400

    working on design

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter = require('./routes/register');
 var loginRouter = require('./routes/login'); 
+var dashboardRouter = require('./routes/dashboard'); 
 
 
 var app = express();
@@ -29,6 +30,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
+app.use('/dashboard', dashboardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
