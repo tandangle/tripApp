6a5fa58764c2681e3331f7f04e74d20829b32226
Tan Le-Dang <tledang@rsglab.com>
2020-05-24 16:54:39 -0400

    need to figure out how to make the axios request asnychronous

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const travelList = require('../models').travelList;
+const axios = require('axios');
 
 /* GET users listing. */
 // router.get("/", function(req, res, next) {
@@ -7,12 +9,27 @@ var router = express.Router();
 //     res.render("dashboard")
 // });
 
-router.get(function(req,res,next) {
+router.get("/", function(req, res) {
+    console.log("Get to Dashboard");
+    console.log(req.user)
     travelList.findAll({
-        where: {user_id: req.user_id}
+        where: {user_id: req.user}
     })
     .then(function (travelList){
-        res.render("dashboard", {user: req.user.firstName, travelList: travelList})
+        console.log(travelList);
+        travelList.forEach(function(item) {
+            axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${item.place_id}&key=AIzaSyDkevaDGz23RoPFkAmtHmOYQQQXwhnfS5E`)
+            .then(function(response){
+                console.log(response.data);
+                placeDetails.push(response.data)
+            })
+            .catch(function(error){
+                console.log(error)
+            })
+        })
+        console.log(placeDetails);
+        console.log(req.user.firstName);
+        res.render("dashboard", {user: req.user.firstName, placeDetails: placeDetails})
     })
 })
 
